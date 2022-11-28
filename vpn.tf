5d3e20082291d5e302952da8dd4864b3e247edf0
markti <markti@outlook.com>
2022-11-28 11:37:27 -0500

    use static ip for vpn

--- vpn.tf
+++ vpn.tf
@@ -3,7 +3,8 @@ resource "azurerm_public_ip" "vpn" {
   name                = "pip-vgw-vpn-${local.suffix}"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
+  sku                 = "Standard"
 
 }
 
